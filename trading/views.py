47b097451420efbb75ebd2031e2316caeae18b16
Jared Clyde <greatwyrm@comcast.net>
2022-12-22 11:59:46 -0700

    created superuser and base templates

--- trading/views.py
+++ trading/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the trading index.")
+    template = loader.get_template('trading/index.html')
+    return render(request, 'trading/index.html')
