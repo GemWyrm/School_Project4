6cb3ac09a737cee686e4bd67f401da3132b39892
Jared Clyde <greatwyrm@comcast.net>
2022-12-22 17:52:45 -0700

    Added yfinance

--- trading/views.py
+++ trading/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
+import yfinance as yf
 
 def index(request):
-    return render(request, 'trading/index.html')
+    data = yf.download("BTC-USD AMZN", period="1mo", group_by="tickers")
+    context = {
+        'data': data,
+    }
+    return render(request, 'trading/index.html', context)
 
 def start(request):
     return redirect('index')
