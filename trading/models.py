39002d5d70dbf08b9b872c08f1a1e871c2bb2ed1
Jared Clyde <greatwyrm@comcast.net>
2022-12-27 10:33:56 -0700

    tried to create graph and display it

--- trading/models.py
+++ trading/models.py
@@ -1,3 +1,4 @@
 from django.db import models
 
-# Create your models here.
+class Chart(models.Model):
+    graph = models.ImageField(default='graph/img.png')
--- trading/views.py
+++ trading/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 import yfinance as yf
+import finplot as fplt
+# import matplotlib.pyplot as plt
+# import matplotlib.dates as mdates
+# import matplotlib.cbook as cbook
 
 def index(request):
-    data = yf.download("BTC-USD AMZN", period="1mo", group_by="tickers")
+    data = yf.download("BTC-USD", period="1mo", group_by="tickers")
+    # This is how to do it in the python shell. How to save and recall this info...
+    # data2 = yf.download("AMZN", period="1mo")
+    fplt.candlestick_ochl(data1[['Open', 'Close', 'High', 'Low']])
+    # fplt.candlestick_ochl(data2[['Open', 'Close', 'High', 'Low']])
+    fplt.save('graph.png', 'BASE_DIR/graph/')
     context = {
         'data': data,
     }
